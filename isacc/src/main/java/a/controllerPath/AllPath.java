c3d244eeb49aa1eb6c8befcd1ba7385f3c320d66
vavogmlfkr3@gmail.com <vavogmlfkr3@gmail.com>
2023-11-28 00:39:07 +0900

    first

--- isacc/src/main/java/a/controllerPath/AllPath.java
+++ isacc/src/main/java/a/controllerPath/AllPath.java
@@ -3,5 +3,5 @@ package a.controllerPath;
 public interface AllPath {
 	final String web = "/WEB-INF/views/";
 	final String home = web+"home.jsp";
-	
+	final String hidden = web+"hidden.jsp";
 }
--- isacc/src/main/java/a/firstController/FirstController.java
+++ isacc/src/main/java/a/firstController/FirstController.java
@@ -1,9 +1,9 @@
 package a.firstController;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import a.controllerPath.AllPath;
 import items.ItemsService;
@@ -18,10 +18,12 @@ public class FirstController implements AllPath{
 	}
 	
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Model model) {
-		
-		return home; 
+	@RequestMapping("/")
+	public String home(HttpServletRequest request) {
+		String key = request.getParameter("key");
+		boolean hiddenPath = key != null && key.equals("admin");
+		if(hiddenPath) return hidden;
+		else return home; 
 	}
 	
 }
